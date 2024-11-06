d2677e7ed50bad5268b581adc33dc10d7cff2d6e
Seungwoo Son <sson68x@gmail.com>
2024-11-05 18:33:17 -0800

    Updated deleted.py

--- delete.py
+++ delete.py
@@ -10,9 +10,26 @@ def deleteWorkout():
 
     try:
         workoutToDelete = int(input("Enter the # to delete: ")) - 1  # Zero-based index
+  
         if 0 <= workoutToDelete < len(workouts):
-            removedWorkout = workouts.pop(workoutToDelete)
-            print(f"Workout '{removedWorkout}' has been removed.")
+            print("Deleting the workout will remove it permanently.")
+            askAgain = int(input("Enter 1 to Continue or 2 to Cancel: "))
+
+            if askAgain == 1:
+                confirmDelete = input("Are you sure you want to delete this workout? (Y/N): ")
+
+                if confirmDelete == "Y" or confirmDelete == "y":
+                    removedWorkout = workouts.pop(workoutToDelete)
+                    print(f"Workout '{removedWorkout}' has been removed.")
+
+                elif confirmDelete == "N" or confirmDelete =="n":
+                    return    
+                    
+                else:
+                    print("Invalid selection.")
+
+            elif askAgain == 2:
+                return                
 
         else:
             print("Invalid selection.")
